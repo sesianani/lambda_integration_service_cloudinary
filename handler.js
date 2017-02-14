16e823d33157f4c22f8c3d9d8cd4b2f8e3459e70
Selasie Anani <selasieanani@gmail.com>
2017-02-14 09:35:45 +0000

    Add ignore rules for npm

--- handler.js
+++ handler.js
@@ -1,8 +1,7 @@
-var eventRunner = require('./app/index.js');
+const eventRunner = require('./app/index.js');
 
 exports.handler = function (event, context) {
     eventRunner.run(event, function(result){
         return context.done(null, result);
     });
-
 };
